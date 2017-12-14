6dc0cc658c00b1d865d12fd1d610cf528f70ef07
Matt <mvt3641@gmail.com>
2017-12-14 13:30:12 -0500

    adding objects

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+//--Objects--//
+$(document).ready(function() {
+	var characters = {
+		"Alien": {
+			name:"Alien",
+			health:150,
+			attack:14,
+			imageurl: "assets/images/alien_xenomorph.jpg",
+			counterAttack: 10
+
+		},
+		"Alien": {
+			name:"Alien",
+			health:150,
+			attack:14,
+			imageurl: "assets/images/alien_xenomorph.jpg",
+			counterAttack: 10
+
+	},
+		"Alien": {
+			name:"Alien",
+			health:150,
+			attack:14,
+			imageurl: "assets/images/alien_xenomorph.jpg",
+			counterAttack: 10
+		},
+		"Alien": {
+			name:"Alien",
+			health:150,
+			attack:14,
+			imageurl: "assets/images/alien_xenomorph.jpg",
+			counterAttack: 10
+
+		}
+
+
+
+
+	}
+
+
