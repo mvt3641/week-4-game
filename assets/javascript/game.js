efc100fe088b7ecbd3dd509d3345a79194c701cf
Matt <mvt3641@gmail.com>
2017-12-14 17:35:22 -0500

    Needs more logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,37 +5,98 @@ $(document).ready(function() {
 			name:"Alien",
 			health:150,
 			attack:14,
-			imageurl: "assets/images/alien_xenomorph.jpg",
+			imageUrl: "assets/images/alien_xenomorph.jpg",
 			counterAttack: 10
 
 		},
-		"Alien": {
-			name:"Alien",
-			health:150,
+		"Predator": {
+			name:"Predator",
+			health:125,
 			attack:14,
-			imageurl: "assets/images/alien_xenomorph.jpg",
+			imageUrl: "assets/images/predator.jpg",
 			counterAttack: 10
 
-	},
-		"Alien": {
-			name:"Alien",
+		},
+		"Queen": {
+			name:"Queen",
 			health:150,
 			attack:14,
-			imageurl: "assets/images/alien_xenomorph.jpg",
+			imageUrl: "assets/images/queen_alien.jpg",
 			counterAttack: 10
 		},
-		"Alien": {
-			name:"Alien",
+		"Colonial Marine": {
+			name:"Colonial Marine",
 			health:150,
 			attack:14,
-			imageurl: "assets/images/alien_xenomorph.jpg",
+			imageUrl: "assets/images/colonial marine.jpg",
 			counterAttack: 10
 
 		}
 
 
+	};
+console.log(characters);
 
 
+
+//Global Varabiles//
+
+var currPlayer;
+var currEnemy;
+var combatants = [];
+
+
+
+//Functions//
+var renderOne = function(character, renderArea, makeChar) {
+// character: obj; renderArea: class/id; makeChar: string
+	var charDiv = $("<div class='character' data-name='" + character.name + "'>");
+    var charName = $("<div class='character-name'>").text(character.name);
+    var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
+    var charHealth = $("<div class='character-health'>").text(character.health);
+    charDiv.append(charName).append(charImage).append(charHealth);
+ 	$(renderArea).append(charDiv)
+
+};
+
+var renderCharacters = function(charObj,areaRender) {
+	if (areaRender == '#character-section') {
+		 $(areaRender).empty();
+      		for (var key in charObj) {
+        		if (charObj.hasOwnProperty(key)) {
+         		 renderOne(charObj[key], areaRender, '');
+       			 }
+     		 }
+    	}
 	}
+
+
+//Render all characters to the page//
+renderCharacters(characters, "#character-section");
+
+
+
+$(document).on({
+		mouseenter: function(){
+			var selector = $(this).css('background-color', '#1d2');
+	
+
+		},
+		mouseleave: function(){
+			var selector = $(this).css('background-color', '#abc')
+
+			
+		}
+	}, '.character');
+
+
+/*$(document).on("click", ".character", function() {
+	
+	var name = $(this).attr("data-name");
+	alert(name);
+
+})*/
+
+});
 
 
